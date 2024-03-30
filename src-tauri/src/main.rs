3d59d5e9fda8aede7c95c60d418c1c22c2da3a3d
tk04 <turkisaloufi@gmail.com>
2024-03-30 10:46:11 -0400

    remove md rust parser

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,8 +4,6 @@ use serde::{Deserialize, Serialize};
 use std::fs::metadata;
 use std::time::SystemTime;
 use tauri::{Manager, Window};
-mod md_parser;
-use md_parser::md_to_html;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FileMeta {
@@ -74,7 +72,7 @@ fn main() {
             Ok(())
         })
         .plugin(tauri_plugin_store::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![get_file_metadata, md_to_html])
+        .invoke_handler(tauri::generate_handler![get_file_metadata])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
